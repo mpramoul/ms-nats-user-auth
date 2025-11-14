57d9c37ffb334180c5778941d7e05cb5901a503c
mpramoul <mpramoul@gmail.com>
2025-11-14 11:21:38 -0500

    ms-nats-user-auth, crud usuarios, register_user ok

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstant } from 'src/jwt/jwtConstants';
 
 @Module({
   imports: [
